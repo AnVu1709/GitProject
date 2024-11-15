8405e61ef0f557a8de504a2789be05c1c5a6b60b
AnVu1709 <chenaholic99er@gmail.com>
2024-11-14 21:22:16 -0800

    gitproject.java

---
+++ src/gitproject.java
@@ -0,0 +1,33 @@
+
+import java.util.Arrays;
+
+public class gitproject {
+
+    // Method to sort an array of integers
+    public void sortArray(int[] array) {
+        Arrays.sort(array);
+        System.out.println("Sorted Integer Array: " + Arrays.toString(array));
+    }
+
+    // Method to sort an array of strings
+    
+    public void sortArray(String[] array) {
+        Arrays.sort(array);
+        System.out.println("Sorted String Array: " + Arrays.toString(array));
+    }
+
+    // Main method for testing
+    public static void main(String[] args) {
+        gitproject gitProjectInstance = new gitproject();
+
+        // Test sorting integers
+        int[] intArray = {4, 2, 7, 1, 9};
+        System.out.println("Original Integer Array: " + Arrays.toString(intArray));
+        gitProjectInstance.sortArray(intArray);
+
+        // Test sorting strings
+        String[] stringArray = {"banana", "Apple", "orange", "apple"};
+        System.out.println("Original String Array: " + Arrays.toString(stringArray));
+        gitProjectInstance.sortArray(stringArray);
+    }
+}
